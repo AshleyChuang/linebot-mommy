c6fea3924ed25996837ed77316137d0b9e86fac4
ashley <ashleyashley19951213@gmail.com>
2018-08-14 23:47:52 +0800

    rich menu

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -11,17 +12,26 @@ from linebot.models import (
     PostbackTemplateAction, MessageTemplateAction,
     URITemplateAction, DatetimePickerTemplateAction,
     ConfirmTemplate, CarouselTemplate, CarouselColumn,
-    ImageCarouselTemplate, ImageCarouselColumn
+    ImageCarouselTemplate, ImageCarouselColumn,
+    RichMenu,RichMenuSize, RichMenuArea, RichMenuBounds
 )
 
 app = Flask(__name__)
 
 # Channel Access Token
-#line_bot_api = LineBotApi('bQu1j0/rJMgNGK++uQcD1Pu8zLcB/Gdp3kwwcwrP4Quj33AGyX1wZtTjPmy8TaNTiEgTgbgzGvN7ZMNtBnDPSubZ0waBtV1tLnbRM7x7O0gkO/z165gjVEJ7YvxoBEMLyzjobckPsfnw5Ncb6hH1fQdB04t89/1O/w1cDnyilFU=')
 line_bot_api = LineBotApi('MIhM1rtBQmPWHHUG0P6WT/q9sOeoe9PTM3NdfLOnI74qp4DtLTHR0WQydDUFbxe868ae78yTpWcRsVQSZJ2FWtV7w+Zqy+Uzomv0jKYFUia8+yT6DKKNd2InF61rFJlQWoPfgeaLzCfQ+JDRNIGFxQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-#handler = WebhookHandler('756c8ca2e53d032ae70d8e1cb6624294')
 handler = WebhookHandler('c80566dca51b314332768ca929117904')
+rich_menu_to_create = RichMenu(
+    size=RichMenuBound(width=2500,height=1686 ),
+    selected=False,
+    name="nice richmenu",
+    chatBarText="touch me",
+    areas=[
+    RichMenuArea(RichMenuBound(x=0,y=0,width=2500,height=1686),URITemplateAction(uri='line://nv/location'))
+    ])
+rich_menu_id = line_bot_api.create_rich_menu(data=rich_menu_to_create)
+print(rich_menu_id)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -47,7 +57,7 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     user_name = profile.display_name
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-import os
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     #crawl_index_movie()
