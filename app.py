44ea1414f28ae0cf1699d410dfa8e90354e5b0c0
Ashley <ashley.chuang@linecorp.com>
2018-08-16 17:55:24 +0800

    test

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def article_fetching(tag):
                 ),
                 URITemplateAction(
                     label='分享',
-                    uri="line://msg/text/?"+article.get('title')+":\n"+article.get('url')
+                    uri=article.get('url')#"line://msg/text/?"+article.get('title')+":\n"+article.get('url')
                 )
             ]
         ))
