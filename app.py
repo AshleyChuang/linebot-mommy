eb7106d42e303ba7f59afbd1b08087c58594c46d
Ashley <ashley.chuang@linecorp.com>
2018-08-16 17:58:26 +0800

    share url

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def article_fetching(tag):
                 ),
                 URITemplateAction(
                     label='分享',
-                    uri=article.get('url')#"line://msg/text/?"+article.get('title')+":\n"+article.get('url')
+                    uri='line://msg/text/?'+'"'+article.get('title')+':\n'+article.get('url')+'"'
                 )
             ]
         ))
